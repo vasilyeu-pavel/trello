dcadeb71ed4904205fa8250fb8b9d86f8f55a793
vasilyeu-pavel <vasilyuepavel@gmail.com>
2018-02-21 23:11:38 +0300

    start state application

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class App extends Component {
+    static propTypes = {
+    };
+
+    render() {
+    	
+        return (
+                <div>
+                	<h2>fdsfsdfsdf</h2>
+                </div>    
+        )
+    }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {boards} from './fixtures'
+import App from './app'
+
+render(<App/>, document.getElementById('container'))

888be16b8c6681e987d61421600609b56e6a4423
vasilyeu-pavel <vasilyuepavel@gmail.com>
2018-02-21 23:24:31 +0300

    start state application

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ class App extends Component {
     };
 
     render() {
-    	
+
         return (
                 <div>
-                	<h2>fdsfsdfsdf</h2>
+                	<h2>123</h2>
                 </div>    
         )
     }
